6a5e906cbe944333cc0e802ae5e23a31702ae81d
Kyle McCormick <kmccormick@edx.org>
2020-04-07 12:01:13 -0400

    Switch to kdmccormick/devstack-frontends branch of frontend-build
    Required doing some reformatting in InsturctorToolbar.jsx
    to make the linter happy.
    
    Also, add dev-build command.

--- src/courseware/course/InstructorToolbar.jsx
+++ src/courseware/course/InstructorToolbar.jsx
@@ -14,14 +14,14 @@ function InstructorToolbar(props) {
       <div className="container-fluid py-3 d-md-flex justify-content-end align-items-center">
         <div className="flex-grow-1">
           <Collapsible.Advanced className="mr-5 mb-md-0">
-              You are currently previewing the new learning sequence experience.
+            You are currently previewing the new learning sequence experience.
             <Collapsible.Trigger className="d-inline-block ml-2" style={{ cursor: 'pointer' }}>
               <Collapsible.Visible whenClosed>
                 <span style={{ borderBottom: 'solid 1px white' }}>More info</span> &rarr;
               </Collapsible.Visible>
             </Collapsible.Trigger>
             <Collapsible.Body>
-                This preview is to allow for early content testing, especially for custom content blocks, with the goal of ensuring it renders as expected in the next experience. You can learn more through the following <a className="text-white" style={{ textDecoration: 'underline' }} href="https://partners.edx.org/announcements/author-preview-learning-sequence-experience-update" target="blank" rel="noopener">Partner Portal post</a>.  Please report any issues or provide <a className="text-white" style={{ textDecoration: 'underline' }} target="blank" rel="noopener" href="https://forms.gle/R6jMYJNTCj1vgC1D6">feedback using the linked form</a>.
+              This preview is to allow for early content testing, especially for custom content blocks, with the goal of ensuring it renders as expected in the next experience. You can learn more through the following <a className="text-white" style={{ textDecoration: 'underline' }} href="https://partners.edx.org/announcements/author-preview-learning-sequence-experience-update" target="blank" rel="noopener">Partner Portal post</a>.  Please report any issues or provide <a className="text-white" style={{ textDecoration: 'underline' }} target="blank" rel="noopener" href="https://forms.gle/R6jMYJNTCj1vgC1D6">feedback using the linked form</a>.
               <Collapsible.Trigger className="d-inline-block ml-2" style={{ cursor: 'pointer' }}>
                 <Collapsible.Visible whenOpen>
                   <span style={{ borderBottom: 'solid 1px white' }}>Close</span> &times;
