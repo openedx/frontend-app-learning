bf0d3b156590b42f58d369269deb361cf124c527
Nick <ndalfonso@edx.org>
2020-06-16 13:11:50 -0400

    AA-133 mfe dates banner fix (#85)
    - mfe dates banner along with fixing previously reverted
      PR by adding back in original coursemetadata call.
    
    This reverts commit 8df4654cf1ac0378aabddafb92db3ebaa51e50ab.

---
+++ src/dates-banner/DatesBanner.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+
+import messages from './messages';
+
+function DatesBanner(props) {
+  const {
+    intl,
+    name,
+    bannerClickHandler,
+  } = props;
+
+  return (
+    <div className="banner rounded my-2 p-3 container-fluid border border-primary-200 bg-info-100">
+      <div className="row justify-content-between">
+        <div className={name === 'datesTabInfoBanner' ? 'col-12' : 'col-9'}>
+          <strong>
+            {intl.formatMessage(messages[`datesBanner.${name}.header`])}
+          </strong>
+          {intl.formatMessage(messages[`datesBanner.${name}.body`])}
+        </div>
+        {bannerClickHandler && (
+          <button type="button" className="btn rounded align-self-center border border-primary bg-white mr-3" onClick={bannerClickHandler}>
+            {intl.formatMessage(messages[`datesBanner.${name}.button`])}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+DatesBanner.propTypes = {
+  intl: intlShape.isRequired,
+  name: PropTypes.string.isRequired,
+  bannerClickHandler: PropTypes.func,
+};
+
+DatesBanner.defaultProps = {
+  bannerClickHandler: null,
+};
+
+export default injectIntl(DatesBanner);
