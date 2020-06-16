8a6745c5a21ae48dc52b987a9db6c5a02e34e203
Nick <xnick421@gmail.com>
2020-06-16 11:13:58 -0400

    AA-133 mfe dates banner fix - mfe dates banner along with fixing previously reverted   PR by adding back in original coursemetadata call.
    This reverts commit 8df4654cf1ac0378aabddafb92db3ebaa51e50ab.

--- src/dates-tab/DatesTab.jsx
+++ src/dates-tab/DatesTab.jsx
@@ -3,10 +3,12 @@ import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 
 import messages from './messages';
 import Timeline from './Timeline';
+import DatesBannerContainer from '../dates-banner/DatesBannerContainer';
 
 function DatesTab({ intl }) {
   return (
     <>
+      <DatesBannerContainer model="dates" />
       <h2 className="mb-4">
         {intl.formatMessage(messages.title)}
       </h2>
