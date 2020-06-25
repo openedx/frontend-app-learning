b05f01d8dd1fcaf5d87b8c7cf07468a21d2738ba
0x29a <demid@opencraft.com>
2020-06-25 15:38:51 +0200

    Add empty tests for courseware and bookmarks

---
+++ src/courseware/course/bookmark/data/redux.test.js
@@ -0,0 +1,21 @@
+describe('Data layer integration tests', () => {
+  describe('Test addBookmark', () => {
+    it('should fail to create bookmark in case of error', () => {
+
+    });
+
+    it('should create bookmark and update model state', () => {
+
+    });
+  });
+
+  describe('Test removeBookmark', () => {
+    it('should fail to remove bookmark in case of error', () => {
+
+    });
+
+    it('should delete bookmark and update model state', () => {
+
+    });
+  });
+});
---
+++ src/courseware/data/redux.test.js
@@ -0,0 +1,21 @@
+describe('Data layer integration tests', () => {
+  describe('Test checkBlockCompletion', () => {
+    it('should fail to check complition in case of error', () => {
+
+    });
+
+    it('should update complete field of unit model', () => {
+
+    });
+  });
+
+  describe('Test saveSequencePosition', () => {
+    it('should change and revert sequence model position in case of error', () => {
+
+    });
+
+    it('should update sequence model position', () => {
+
+    });
+  });
+});
