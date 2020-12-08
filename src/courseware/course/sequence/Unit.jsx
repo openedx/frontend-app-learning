e7e192585f99e8d2c8d0a1dc139c903f918a9bfd
David Joy <djoy@edx.org>
2020-12-08 14:49:34 -0500

    Set styling for Unit title to h3.

--- src/courseware/course/sequence/Unit.jsx
+++ src/courseware/course/sequence/Unit.jsx
@@ -111,7 +111,7 @@ function Unit({
 
   return (
     <div className="unit">
-      <h2 className="mb-0 h4">{unit.title}</h2>
+      <h2 className="mb-0 h3">{unit.title}</h2>
       <BookmarkButton
         unitId={unit.id}
         isBookmarked={unit.bookmarked}
