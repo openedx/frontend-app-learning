b57c897a2283171119db56f9d6969c259e474cd9
David Joy <djoy@edx.org>
2020-12-08 14:34:48 -0500

    Removing underline from “Help” button in header

--- src/course-header/Header.jsx
+++ src/course-header/Header.jsx
@@ -83,7 +83,7 @@ function Header({
           <span className="d-block small m-0">{courseOrg} {courseNumber}</span>
           <span className="d-block m-0 font-weight-bold course-title">{courseTitle}</span>
         </div>
-        <a className="text-gray-700 mr-3" href={`${getConfig().SUPPORT_URL}`}>{intl.formatMessage(messages.help)}</a>
+        <a className="text-gray-700 mr-3 text-decoration-none" href={`${getConfig().SUPPORT_URL}`}>{intl.formatMessage(messages.help)}</a>
         <Dropdown className="user-dropdown">
           <Dropdown.Toggle variant="light">
             <FontAwesomeIcon icon={faUserCircle} className="d-md-none" size="lg" />
