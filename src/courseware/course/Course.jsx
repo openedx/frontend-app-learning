9e0f5d7e2290c46678254fd7ec3705d2d09fe0af
Emma Green <emma.green.software@gmail.com>
2021-01-02 22:15:16 -0500

    add black underline when value prop flyover is open

--- src/courseware/course/Course.jsx
+++ src/courseware/course/Course.jsx
@@ -99,6 +99,7 @@ function Course({
         sequenceId={sequenceId}
         toggleREV1512Flyover={toggleREV1512Flyover} /* This line should be reverted after REV1512 experiment */
         REV1512FlyoverEnabled={REV1512FlyoverEnabled} /* This line should be reverted after REV1512 experiment */
+        isREV1512FlyoverVisible={isREV1512FlyoverVisible} /* This line should be reverted after REV1512 experiment */
       />
       <AlertList topic="sequence" />
       <Sequence
