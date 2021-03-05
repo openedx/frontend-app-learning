661d71e2543837fc3d86f70641f5fa99e12b61a8
Diane Kaplan <dkaplan@edx.org>
2021-03-05 13:46:34 -0500

    update wording for existing strings

--- src/courseware/course/sequence/lock-paywall/messages.js
+++ src/courseware/course/sequence/lock-paywall/messages.js
@@ -3,17 +3,17 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'learn.lockPaywall.title': {
     id: 'learn.lockPaywall.title',
-    defaultMessage: 'Verified Track Access',
+    defaultMessage: 'Graded assignments are locked',
     description: 'Heading for message shown to indicate that a piece of content is unavailable to audit track users.',
   },
   'learn.lockPaywall.content': {
     id: 'learn.lockPaywall.content',
-    defaultMessage: 'Graded assessments are available to Verified Track learners.',
+    defaultMessage: 'Upgrade to gain access to locked features like this one and get the most out of your course.',
     description: 'Message shown to indicate that a piece of content is unavailable to audit track users.',
   },
   'learn.lockPaywall.upgrade.link': {
     id: 'learn.lockPaywall.upgrade.link',
-    defaultMessage: 'Upgrade to unlock ({currencySymbol}{price})',
+    defaultMessage: 'Upgrade for ({currencySymbol}{price})',
     description: 'A link users can click that navigates their browser to the upgrade payment page.',
   },
   'learn.lockPaywall.example.alt': {
