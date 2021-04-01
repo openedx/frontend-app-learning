6abf8531bb64d3f4581eef3f01a98795a9870e23
Diane Kaplan <dianekaplan@gmail.com>
2021-04-01 10:35:19 -0400

    feat: ahead of value prop designs, update text to prep translations (REV-2127)
    Co-authored-by: Diane Kaplan <dkaplan@edx.org>

--- src/courseware/course/sequence/lock-paywall/messages.js
+++ src/courseware/course/sequence/lock-paywall/messages.js
@@ -3,23 +3,48 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'learn.lockPaywall.title': {
     id: 'learn.lockPaywall.title',
-    defaultMessage: 'Verified Track Access',
+    defaultMessage: 'Graded assignments are locked',
     description: 'Heading for message shown to indicate that a piece of content is unavailable to audit track users.',
   },
   'learn.lockPaywall.content': {
     id: 'learn.lockPaywall.content',
-    defaultMessage: 'Graded assessments are available to Verified Track learners.',
+    defaultMessage: 'Upgrade to gain access to locked features like this one and get the most out of your course.',
     description: 'Message shown to indicate that a piece of content is unavailable to audit track users.',
   },
   'learn.lockPaywall.upgrade.link': {
     id: 'learn.lockPaywall.upgrade.link',
-    defaultMessage: 'Upgrade to unlock ({currencySymbol}{price})',
+    defaultMessage: 'Upgrade for {currencySymbol}{price}',
     description: 'A link users can click that navigates their browser to the upgrade payment page.',
   },
   'learn.lockPaywall.example.alt': {
     id: 'learn.lockPaywall.example.alt',
     defaultMessage: 'Example Certificate',
     description: 'Alternate text displayed when the example certificate image cannot be displayed.',
+  },
+  'learn.lockPaywall.list.intro': {
+    id: 'learn.lockPaywall.list.intro',
+    defaultMessage: 'When you upgrade, you:',
+    description: 'Text displayed to introduce the list of benefits from upgrading.',
+  },
+  'learn.lockPaywall.list.bullet1.linktext': {
+    id: 'learn.lockPaywall.list.bullet1.linktext',
+    defaultMessage: 'verified certificate',
+    description: 'Link text for verified certificate info page.',
+  },
+  'learn.lockPaywall.list.bullet2.boldtext': {
+    id: 'learn.lockPaywall.list.bullet2.boldtext',
+    defaultMessage: 'graded assignments',
+    description: 'Bolded text for graded assignments.',
+  },
+  'learn.lockPaywall.list.bullet3.boldtext': {
+    id: 'learn.lockPaywall.list.bullet3.boldtext',
+    defaultMessage: 'Full access',
+    description: 'Bolded text for full access.',
+  },
+  'learn.lockPaywall.list.bullet4.boldtext': {
+    id: 'learn.lockPaywall.list.bullet4.boldtext',
+    defaultMessage: 'non-profit',
+    description: 'Bolded text to highlight our non-profit status.',
   },
 });
 
