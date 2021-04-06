52a21424c3621092fb7c5239cc89e22f562c5956
julianajlk <julianajlk@gmail.com>
2021-04-05 20:36:18 -0400

    Move notification icon and red dot to separate component

--- src/courseware/course/SidebarNotificationButton.jsx
+++ src/courseware/course/SidebarNotificationButton.jsx
@@ -2,27 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { Icon } from '@edx/paragon';
-import { WatchOutline } from '@edx/paragon/icons';
 
+import NotificationIcon from './NotificationIcon';
 import './SidebarNotificationButton.scss';
 import messages from './messages';
 
 function SidebarNotificationButton({ intl, toggleSidebar, isSidebarVisible }) {
   return (
-    <div
-      className={classNames('sidebar-notification-btn-container', { active: isSidebarVisible() })}
+    <button
+      className={classNames('sidebar-notification-btn', { active: isSidebarVisible() })}
+      type="button"
       data-testid="SidebarButton"
-      role="button"
-      tabIndex="0"
       onClick={() => { toggleSidebar(); }}
-      onKeyDown={() => { toggleSidebar(); }}
       alt={intl.formatMessage(messages.notificationButton)}
     >
-      <Icon src={WatchOutline} className="sidebar-notification-icon" alt={intl.formatMessage(messages.notificationButton)} />
-      {/* JK: add conditional for icon with red dot */}
-      <span className="notification-dot p-1" />
-    </div>
+      <NotificationIcon status="active" notificationColor="bg-danger-500" />
+    </button>
   );
 }
 
