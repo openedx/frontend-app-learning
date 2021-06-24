ed81b8c40af71d6701608c6785fb39b7a4d6d068
Thomas Tracy <contact@tjtracy.com>
2021-06-24 14:26:18 -0400

    refactor alert render code

--- src/course-home/outline-tab/alerts/certificate-status-alert/CertificateStatusAlert.jsx
+++ src/course-home/outline-tab/alerts/certificate-status-alert/CertificateStatusAlert.jsx
@@ -29,82 +29,109 @@ function CertificateStatusAlert({ intl, payload }) {
     userTimezone,
   } = payload;
 
-  let variant = '';
-  let icon = '';
-  let iconClassName = '';
-  if (certStatusType === CERT_STATUS_TYPE.EARNED_NOT_AVAILABLE || certStatusType === CERT_STATUS_TYPE.DOWNLOADABLE) {
-    variant = 'success';
-    icon = faCheckCircle;
-    iconClassName = 'alert-icon text-success-500';
-  }
+  // eslint-disable-next-line react/prop-types
+  const AlertWrapper = (props) => props.children(props);
 
-  if (certStatusType === CERT_STATUS_TYPE.UNVERIFIED) {
-    variant = 'warning';
-    icon = faExclamationTriangle;
-    iconClassName = 'alert-icon text-warning-500';
-  }
-
-  let header = '';
-  let body = '';
-  let buttonVisible = false;
-  let buttonMessage = '';
-  let buttonLink = '';
-  if (certStatusType === CERT_STATUS_TYPE.EARNED_NOT_AVAILABLE) {
-    const timezoneFormatArgs = userTimezone ? { timeZone: userTimezone } : {};
-    const certificateAvailableDateFormatted = <FormattedDate value={certificateAvailableDate} day="numeric" month="long" year="numeric" />;
-    const courseEndDateFormatted = <FormattedDate value={courseEndDate} day="numeric" month="long" year="numeric" />;
-
-    header = intl.formatMessage(certMessages.certStatusEarnedNotAvailableHeader);
-    body = (
-      <p>
-        <FormattedMessage
-          id="learning.outline.alert.cert.when"
-          defaultMessage="This course ended on {courseEndDateFormatted}. Final grades and certificates are
-          scheduled to be available after {certificateAvailableDate}."
-          values={{
-            courseEndDateFormatted,
-            certificateAvailableDate: certificateAvailableDateFormatted,
-          }}
-          {...timezoneFormatArgs}
-        />
-      </p>
-    );
-  } else if (certStatusType === CERT_STATUS_TYPE.DOWNLOADABLE) {
-    header = intl.formatMessage(certMessages.certStatusDownloadableHeader);
-    if (isWebCert) {
-      buttonMessage = intl.formatMessage(certStatusMessages.viewableButton);
-    } else {
-      buttonMessage = intl.formatMessage(certStatusMessages.downloadableButton);
+  const renderCertAwardedStatus = () => {
+    const alertProps = {
+      variant: 'success',
+      icon: faCheckCircle,
+      iconClassName: 'alert-icon text-success-500',
+    };
+    if (certStatusType === CERT_STATUS_TYPE.EARNED_NOT_AVAILABLE) {
+      const timezoneFormatArgs = userTimezone ? { timeZone: userTimezone } : {};
+      const certificateAvailableDateFormatted = <FormattedDate value={certificateAvailableDate} day="numeric" month="long" year="numeric" />;
+      const courseEndDateFormatted = <FormattedDate value={courseEndDate} day="numeric" month="long" year="numeric" />;
+      alertProps.header = intl.formatMessage(certMessages.certStatusEarnedNotAvailableHeader);
+      alertProps.body = (
+        <p>
+          <FormattedMessage
+            id="learning.outline.alert.cert.when"
+            defaultMessage="This course ended on {courseEndDateFormatted}. Final grades and certificates are
+            scheduled to be available after {certificateAvailableDate}."
+            values={{
+              courseEndDateFormatted,
+              certificateAvailableDate: certificateAvailableDateFormatted,
+            }}
+            {...timezoneFormatArgs}
+          />
+        </p>
+      );
+    } else if (certStatusType === CERT_STATUS_TYPE.DOWNLOADABLE) {
+      alertProps.header = intl.formatMessage(certMessages.certStatusDownloadableHeader);
+      if (isWebCert) {
+        alertProps.buttonMessage = intl.formatMessage(certStatusMessages.viewableButton);
+      } else {
+        alertProps.buttonMessage = intl.formatMessage(certStatusMessages.downloadableButton);
+      }
+      alertProps.buttonVisible = true;
+      alertProps.buttonLink = certURL;
     }
-    buttonVisible = true;
-    buttonLink = certURL;
-  } else if (certStatusType === CERT_STATUS_TYPE.UNVERIFIED) {
-    header = intl.formatMessage(certStatusMessages.unverifiedHomeHeader);
-    buttonMessage = intl.formatMessage(certStatusMessages.unverifiedHomeButton);
-    body = intl.formatMessage(certStatusMessages.unverifiedHomeBody);
-    buttonVisible = true;
-    buttonLink = getConfig().SUPPORT_URL_ID_VERIFICATION;
+    return alertProps;
+  };
+
+  const renderNotIDVerifiedStatus = () => {
+    const alertProps = {
+      variant: 'warning',
+      icon: faExclamationTriangle,
+      iconClassName: 'alert-icon text-warning-500',
+      header: intl.formatMessage(certStatusMessages.unverifiedHomeHeader),
+      buttonMessage: intl.formatMessage(certStatusMessages.unverifiedHomeButton),
+      body: intl.formatMessage(certStatusMessages.unverifiedHomeBody),
+      buttonVisible: true,
+      buttonLink: getConfig().SUPPORT_URL_ID_VERIFICATION,
+    };
+
+    return alertProps;
+  };
+
+  let alertProps = {};
+  switch (certStatusType) {
+    case CERT_STATUS_TYPE.EARNED_NOT_AVAILABLE:
+    case CERT_STATUS_TYPE.DOWNLOADABLE:
+      alertProps = renderCertAwardedStatus();
+      break;
+    case CERT_STATUS_TYPE.UNVERIFIED:
+      alertProps = renderNotIDVerifiedStatus();
+      break;
+    default:
+      break;
   }
+
   return (
-    <Alert variant={variant}>
-      <div className="row justify-content-between align-items-center">
-        <div className={buttonVisible ? 'col-lg-8' : 'col-auto'}>
-          <FontAwesomeIcon icon={icon} className={iconClassName} />
-          <Alert.Heading>{header}</Alert.Heading>
-          {body}
-        </div>
-        {buttonVisible && (
-          <div className="m-auto m-lg-0 pr-lg-3">
-            <Button
-              variant="primary"
-              href={buttonLink}
-            >
-              {buttonMessage}
-            </Button>
+    <AlertWrapper {...alertProps}>
+      {({
+        variant,
+        buttonVisible,
+        iconClassName,
+        icon,
+        header,
+        buttonLink,
+        body,
+        buttonMessage,
+      }) => (
+        <Alert variant={variant}>
+          <div className="row justify-content-between align-items-center">
+            <div className={buttonVisible ? 'col-lg-8' : 'col-auto'}>
+              <FontAwesomeIcon icon={icon} className={iconClassName} />
+              <Alert.Heading>{header}</Alert.Heading>
+              {body}
+            </div>
+            {buttonVisible && (
+              <div className="m-auto m-lg-0 pr-lg-3">
+                <Button
+                  variant="primary"
+                  href={buttonLink}
+                >
+                  {buttonMessage}
+                </Button>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </Alert>
+        </Alert>
+
+      )}
+    </AlertWrapper>
   );
 }
 
