f21dad95b5d2120972185496531d3d0589031acf
Sagirov Evgeniy <34642612+UvgenGen@users.noreply.github.com>
2021-07-12 18:12:43 +0300

    feat: [BD-26] Timer bar on non-sequence pages (#502)
    * feat: Timer bar on non-sequence pages
    
    * chore: Update frontend-lib-special-exams version.
    
    Co-authored-by: Viktor Rusakov <vrusakov66@gmail.com>
    Co-authored-by: Igor Degtiarov <igor.degtiarov@raccoongang.com>

--- src/tab-page/TabContainer.jsx
+++ src/tab-page/TabContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { OuterExamTimer } from '@edx/frontend-lib-special-exams';
 
 import TabPage from './TabPage';
 
@@ -34,6 +35,7 @@ export default function TabContainer(props) {
       courseStatus={courseStatus}
       metadataModel={`${slice}Meta`}
     >
+      {courseId && <OuterExamTimer courseId={courseId} />}
       {children}
     </TabPage>
   );
