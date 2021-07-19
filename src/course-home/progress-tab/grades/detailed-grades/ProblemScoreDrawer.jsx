057b431818e0ff89afc110796c1418677ec89e04
Carla Duarte <cduarte@edx.org>
2021-07-19 13:47:59 -0400

    fix: problem score styling (#546)

--- src/course-home/progress-tab/grades/detailed-grades/ProblemScoreDrawer.jsx
+++ src/course-home/progress-tab/grades/detailed-grades/ProblemScoreDrawer.jsx
@@ -7,13 +7,15 @@ import messages from '../messages';
 
 function ProblemScoreDrawer({ intl, problemScores }) {
   return (
-    <span className="row w-100 m-0 x-small ml-4 pt-2 pl-1 text-gray-700">
-      <span id="problem-score-label">{intl.formatMessage(messages.problemScoreLabel)}</span>
-      <ul className="list-unstyled row m-0" aria-labelledby="problem-score-label">
-        {problemScores.map(problemScore => (
-          <li className="ml-3">{problemScore.earned}/{problemScore.possible}</li>
-        ))}
-      </ul>
+    <span className="row w-100 m-0 x-small ml-4 pt-2 pl-1 text-gray-700 flex-nowrap">
+      <span id="problem-score-label" className="col-auto p-0">{intl.formatMessage(messages.problemScoreLabel)}</span>
+      <div className="col p-0">
+        <ul className="list-unstyled row w-100 m-0" aria-labelledby="problem-score-label">
+          {problemScores.map(problemScore => (
+            <li className="ml-3">{problemScore.earned}/{problemScore.possible}</li>
+          ))}
+        </ul>
+      </div>
     </span>
   );
 }
