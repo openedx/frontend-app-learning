35ff4ae2f17ced35b32c52eda2945c94291f9794
Matthew Piatetsky <mpiatetsky@edx.org>
2021-07-27 11:43:01 -0400

    fix: grey out problem scores when a learner does not have access, instead of when a learner does have access

--- src/course-home/progress-tab/grades/detailed-grades/ProblemScoreDrawer.jsx
+++ src/course-home/progress-tab/grades/detailed-grades/ProblemScoreDrawer.jsx
@@ -10,7 +10,7 @@ function ProblemScoreDrawer({ intl, problemScores, subsection }) {
   return (
     <span className="row w-100 m-0 x-small ml-4 pt-2 pl-1 text-gray-700 flex-nowrap">
       <span id="problem-score-label" className="col-auto p-0">{intl.formatMessage(messages.problemScoreLabel)}</span>
-      <div className={classNames('col', 'p-0', { 'greyed-out': subsection.learnerHasAccess })}>
+      <div className={classNames('col', 'p-0', { 'greyed-out': !subsection.learnerHasAccess })}>
         <ul className="list-unstyled row w-100 m-0" aria-labelledby="problem-score-label">
           {problemScores.map(problemScore => (
             <li className="ml-3">{problemScore.earned}/{problemScore.possible}</li>
