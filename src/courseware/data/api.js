c6578d4e2ec90fee7b96477e3eb6bfc31e5a0db3
Kristin Aoki <kaoki@edx.org>
2021-08-12 16:20:07 -0400

    Update url to take direct sequence id

--- src/courseware/data/api.js
+++ src/courseware/data/api.js
@@ -283,7 +283,7 @@ export async function getSequenceMetadata(sequenceId) {
   return normalizeSequenceMetadata(data);
 }
 
-const getSequenceHandlerUrl = (courseId, sequenceId) => `${getConfig().LMS_BASE_URL}/courses/${courseId}/xblock/${sequenceId.decoded_id}/handler`;
+const getSequenceHandlerUrl = (courseId, sequenceId) => `${getConfig().LMS_BASE_URL}/courses/${courseId}/xblock/${sequenceId}/handler`;
 
 export async function getBlockCompletion(courseId, sequenceId, usageKey) {
   const { data } = await getAuthenticatedHttpClient().post(
