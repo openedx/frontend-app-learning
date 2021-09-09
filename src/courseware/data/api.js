d6fa139833e6eca36f6e670a6d91ae6a71cba4bc
Michael Terry <mterry@edx.org>
2021-09-09 09:17:31 -0400

    feat: notify the user if a sequence is hidden because of due date
    Normally, these sequences are skipped. But if the user manually
    goes to the section, they should be notified why they can't access
    it. That can easily happen if they bookmarked the page or something.
    
    AA-1000

--- src/courseware/data/api.js
+++ src/courseware/data/api.js
@@ -253,6 +253,7 @@ function normalizeSequenceMetadata(sequence) {
       gatedContent: camelCaseObject(sequence.gated_content),
       isTimeLimited: sequence.is_time_limited,
       isProctored: sequence.is_proctored,
+      isHiddenAfterDue: sequence.is_hidden_after_due,
       // Position comes back from the server 1-indexed. Adjust here.
       activeUnitIndex: sequence.position ? sequence.position - 1 : 0,
       saveUnitPosition: sequence.save_position,
