1a4b26b5cd26e4704168aae87fc7045e450f3d2f
Thomas Tracy <contact@tjtracy.com>
2021-10-06 14:02:40 -0400

    Add error logging.

--- src/generic/notices/api.js
+++ src/generic/notices/api.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import { getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient, getAuthenticatedUser } from '@edx/frontend-platform/auth';
+import { logError, logInfo } from '@edx/frontend-platform/logging';
 
 export const getNotices = async () => {
   const authenticatedUser = getAuthenticatedUser();
@@ -12,6 +13,12 @@ export const getNotices = async () => {
     } catch (e) {
       // we will just swallow error, as that probably means the notices app is not installed.
       // Notices are not necessary for the rest of courseware to function.
+      const { customAttributes: { httpErrorStatus } } = e;
+      if (httpErrorStatus === 404) {
+        logInfo(`${e}. This probably happened because the notices plugin is not installed on platform.`);
+      } else {
+        logError(e);
+      }
     }
   }
   return null;
