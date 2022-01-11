9dfd3369cce97a7709d9ef4e8720d0c4b7fc197f
Dillon Dumesnil <ddumesnil@edx.org>
2022-01-11 16:29:11 -0500

    feat: AA-1138: Adds in Weekly Goal Celebration Modal
    The logic to show the modal is controlled by the backend.
    Displays the modal only in courseware the first time the learner
    hits their weekly learning goal. After viewing the goal, the
    database row is updated to not show the modal again.
    
    Also updates first section celebration to use the StandardModal
    component as the Modal component has been deprecated.

--- src/courseware/course/Course.test.jsx
+++ src/courseware/course/Course.test.jsx
@@ -65,11 +65,7 @@ describe('Course', () => {
     );
   });
 
-  it('displays celebration modal', async () => {
-    // TODO: Remove these console mocks after merging https://github.com/edx/paragon/pull/526.
-    jest.spyOn(console, 'warn').mockImplementation(() => {});
-    jest.spyOn(console, 'error').mockImplementation(() => {});
-
+  it('displays first section celebration modal', async () => {
     const courseMetadata = Factory.build('courseMetadata', { celebrations: { firstSection: true } });
     const testStore = await initializeTestStore({ courseMetadata }, false);
     const { courseware, models } = testStore.getState();
@@ -84,9 +80,27 @@ describe('Course', () => {
     handleNextSectionCelebration(sequenceId, sequenceId, testData.unitId);
     render(<Course {...testData} />, { store: testStore });
 
-    const celebrationModal = screen.getByRole('dialog');
-    expect(celebrationModal).toBeInTheDocument();
-    expect(getByRole(celebrationModal, 'heading', { name: 'Congratulations!' })).toBeInTheDocument();
+    const firstSectionCelebrationModal = screen.getByRole('dialog');
+    expect(firstSectionCelebrationModal).toBeInTheDocument();
+    expect(getByRole(firstSectionCelebrationModal, 'heading', { name: 'Congratulations!' })).toBeInTheDocument();
+  });
+
+  it('displays weekly goal celebration modal', async () => {
+    const courseMetadata = Factory.build('courseMetadata', { celebrations: { weeklyGoal: true } });
+    const testStore = await initializeTestStore({ courseMetadata }, false);
+    const { courseware, models } = testStore.getState();
+    const { courseId, sequenceId } = courseware;
+    const testData = {
+      ...mockData,
+      courseId,
+      sequenceId,
+      unitId: Object.values(models.units)[0].id,
+    };
+    render(<Course {...testData} />, { store: testStore });
+
+    const weeklyGoalCelebrationModal = screen.getByRole('dialog');
+    expect(weeklyGoalCelebrationModal).toBeInTheDocument();
+    expect(getByRole(weeklyGoalCelebrationModal, 'heading', { name: 'You met your goal!' })).toBeInTheDocument();
   });
 
   it('displays notification trigger and toggles active class on click', async () => {
@@ -171,8 +185,8 @@ describe('Course', () => {
     loadUnit();
     await waitFor(() => expect(screen.queryByText('Loading learning sequence...')).not.toBeInTheDocument());
     // expect the section and sequence "titles" to be loaded in as breadcrumb labels.
-    expect(screen.getByText('cdabcdabcdabcdabcdabcdabcdabcd13')).toBeInTheDocument();
-    expect(screen.getByText('cdabcdabcdabcdabcdabcdabcdabcd12')).toBeInTheDocument();
+    expect(screen.getByText(Object.values(models.sections)[0].title)).toBeInTheDocument();
+    expect(screen.getByText(Object.values(models.sequences)[0].title)).toBeInTheDocument();
   });
 
   it('passes handlers to the sequence', async () => {
