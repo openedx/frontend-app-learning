5cad5eaebb8f71a30e202c4173a78521dd3b46a9
hammadahmadwaqas <hammadahmadwaqas1@gmail.com>
2022-07-18 15:44:51 +0500

    feat: Added support to redirect to DSC if required.

---
+++ src/courseware/CoursewareRedirectLandingPage.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import CoursewareRedirectLandingPage from './CoursewareRedirectLandingPage';
+
+const redirectUrl = jest.fn();
+
+jest.mock('@edx/frontend-platform/analytics');
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useRouteMatch: () => ({
+    path: '/redirect',
+  }),
+}));
+
+describe('CoursewareRedirectLandingPage', () => {
+  beforeEach(async () => {
+    delete global.location;
+    global.location = { assign: redirectUrl };
+  });
+
+  it('Redirects to correct consent URL', () => {
+    const history = createMemoryHistory({
+      initialEntries: ['/redirect/consent/?consentPath=%2Fgrant_data_sharing_consent'],
+    });
+
+    render(
+      <Router history={history}>
+        <CoursewareRedirectLandingPage />
+      </Router>,
+    );
+
+    expect(redirectUrl).toHaveBeenCalledWith('http://localhost:18000/grant_data_sharing_consent');
+  });
+});
