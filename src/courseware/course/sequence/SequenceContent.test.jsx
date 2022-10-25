acaa7cea7c51025b921cfcf133e7023219d083e0
Diana Catalina Olarte <diana.olarte@edunext.co>
2022-10-25 11:45:10 -0500

    fix: single quotes rendering

--- src/courseware/course/sequence/SequenceContent.test.jsx
+++ src/courseware/course/sequence/SequenceContent.test.jsx
@@ -32,7 +32,7 @@ describe('Sequence Content', () => {
     expect(screen.queryByText('Loading locked content messaging...')).not.toBeInTheDocument();
     expect(container.querySelector('svg')).toHaveClass('fa-lock');
     expect(screen.getByText(
-      `You must complete the prerequisite: ' ${gatedContent.prereqSectionName} ' to access this content.`,
+      `You must complete the prerequisite: '${gatedContent.prereqSectionName}' to access this content.`,
     )).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Go To Prerequisite Section' })).toBeInTheDocument();
   });
