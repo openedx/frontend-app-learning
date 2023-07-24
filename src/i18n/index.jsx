2107debaf3ecc8fdb5d7cfa12ae320616a3a157a
milad-emami <milad.emami1025@gmail.com>
2023-07-24 13:26:52 +0330

    feat: persian support added

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,4 +1,5 @@
 import arMessages from './messages/ar.json';
+import faIRMessages from './messages/fa.json';
 import caMessages from './messages/ca.json';
 // no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
@@ -15,6 +16,7 @@ import ukMessages from './messages/uk.json';
 
 const messages = {
   ar: arMessages,
+  fa: faIRMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
