2b02fcf361da937fba1cbdfa9bf34bddb27b63d5
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-08-17 13:00:05 -0600

    Added patch-package and patch for frontend-lib-special-exams (remove exports part in package.json because it causes issues)

--- src/exports.js
+++ src/exports.js
@@ -1,3 +1,4 @@
-import Sequence from './courseware/course/sequence';
+import Sequence from "./courseware/course/sequence";
+import CompleteIcon from "./courseware/course/sequence/sequence-navigation/CompleteIcon";
 
-export { Sequence };
+export { Sequence, CompleteIcon };
