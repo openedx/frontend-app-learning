e109da8590d72e487fc0c050ad65aaf976817fd5
michaelroytman <mroytman@edx.org>
2024-02-06 14:34:48 -0500

    feat: refactor use of frontend-lib-special-exams public API to hooks
    This commit refactors the use of the frontend-lib-special-exams public API to use hooks.
    
    This commit also imports the root reducer from the frontend-lib-special-exams library. This root reducer is used for the specialExams slice when configuring the store for this application.

--- src/courseware/course/sequence/Unit/hooks/useExamAccess.js
+++ src/courseware/course/sequence/Unit/hooks/useExamAccess.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { logError } from '@edx/frontend-platform/logging';
 import { StrictDict, useKeyedState } from '@edx/react-unit-test-utils';
-import { getExamAccess, fetchExamAccess, isExam } from '@edx/frontend-lib-special-exams';
+import { useExamAccessToken, useFetchExamAccessToken, useIsExam } from '@edx/frontend-lib-special-exams';
 
 export const stateKeys = StrictDict({
   accessToken: 'accessToken',
@@ -12,14 +12,23 @@ export const stateKeys = StrictDict({
 const useExamAccess = ({
   id,
 }) => {
-  const [accessToken, setAccessToken] = useKeyedState(stateKeys.accessToken, '');
-  const [blockAccess, setBlockAccess] = useKeyedState(stateKeys.blockAccess, isExam());
+  const isExam = useIsExam();
+  const [blockAccess, setBlockAccess] = useKeyedState(stateKeys.blockAccess, isExam);
+
+  const fetchExamAccessToken = useFetchExamAccessToken();
+
+  // NOTE: We cannot use this hook in the useEffect hook below to grab the updated exam access token in the finally
+  //       block, due to the rules of hooks. Instead, we get the value of the exam access token from a call to the hook.
+  //       When the fetchExamAccessToken call completes, the useExamAccess hook will re-run
+  //       (due to a change to the Redux store, and, thus, a change to the the context), at which point the updated
+  //       exam access token will be fetched via the useExamAccessToken hook call below.
+  //       The important detail is that there should never be a return value (false, '').
+  const examAccessToken = useExamAccessToken();
+
   React.useEffect(() => {
-    if (isExam()) {
-      fetchExamAccess()
+    if (isExam) {
+      fetchExamAccessToken()
         .finally(() => {
-          const examAccess = getExamAccess();
-          setAccessToken(examAccess);
           setBlockAccess(false);
         })
         .catch((error) => {
@@ -30,7 +39,7 @@ const useExamAccess = ({
 
   return {
     blockAccess,
-    accessToken,
+    accessToken: examAccessToken,
   };
 };
 
