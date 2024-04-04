26f53ccfbdf15d8d2023a4f2ebe5c09637e4d133
Muhammad Abdullah Waheed <abdullah.waheed@arbisoft.com>
2024-04-04 14:58:13 +0500

    refactor: added datadog logging

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import { AppProvider, ErrorPage, PageWrap } from '@edx/frontend-platform/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Routes, Route } from 'react-router-dom';
+import { datadogRum } from '@datadog/browser-rum';
 
 import { Helmet } from 'react-helmet';
 import { fetchDiscussionTab, fetchLiveTab } from './course-home/data/thunks';
@@ -39,6 +40,21 @@ import DecodePageRoute from './decode-page-route';
 import { DECODE_ROUTES, ROUTES } from './constants';
 
 subscribe(APP_READY, () => {
+  datadogRum.init({
+    applicationId: 'a3f99dcb-4955-4baa-8341-39a88603ab08',
+    clientToken: 'pubf2e79d946cec4c4413965620ba0e0b72',
+    site: 'datadoghq.com',
+    service: 'edx-frontend-sandbox',
+    env: 'staging',
+    // Specify a version number to identify the deployed version of your application in Datadog
+    version: '1.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  });
   ReactDOM.render(
     <AppProvider store={initializeStore()}>
       <Helmet>
