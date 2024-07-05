a9ae7ea9025aa02099eaa75629dadcf7da7d7e9f
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2024-07-05 16:04:34 +0500

    refactor: added code comment in jest config

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const config = createConfig('jest', {
     // See https://stackoverflow.com/questions/72382316/jest-encountered-an-unexpected-token-react-markdown
     'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
     '@src/(.*)': '<rootDir>/src/$1',
+    // Explicit mapping to ensure Jest resolves the module correctly
     '@edx/frontend-lib-special-exams': '<rootDir>/node_modules/@edx/frontend-lib-special-exams',
   },
   testTimeout: 30000,
