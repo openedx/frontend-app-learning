660f53f2c77e34c3e1af69d1073b2013a6db5018
Peter Kulko <peter.kulko@raccoongang.com>
2024-08-29 10:31:45 +0300

    feat: [AsPu-731] increased font-size for Progress page

--- src/course-home/progress-tab/course-completion/CourseCompletion.jsx
+++ src/course-home/progress-tab/course-completion/CourseCompletion.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import classNames from 'classnames';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { breakpoints, useWindowSize } from '@edx/paragon';
 
 import CompletionDonutChart from './CompletionDonutChart';
 import messages from './messages';
 
-const CourseCompletion = ({ intl }) => (
-  <section className="text-dark-700 mb-4 rounded raised-card p-4">
-    <div className="row w-100 m-0">
-      <div className="col-12 col-sm-6 col-md-7 p-0">
-        <h2>{intl.formatMessage(messages.courseCompletion)}</h2>
-        <p className="small">
-          {intl.formatMessage(messages.completionBody)}
-        </p>
+const CourseCompletion = ({ intl }) => {
+  const wideScreen = useWindowSize().width >= breakpoints.medium.minWidth;
+
+  return (
+    <section className={classNames('text-dark-700 mb-4 rounded raised-card p-4', { small: !wideScreen })}>
+      <div className="row w-100 m-0">
+        <div className="col-12 col-sm-6 col-md-7 p-0">
+          <h2>{intl.formatMessage(messages.courseCompletion)}</h2>
+          <p>
+            {intl.formatMessage(messages.completionBody)}
+          </p>
+        </div>
+        <div className="col-12 col-sm-6 col-md-5 mt-sm-n3 p-0 text-center">
+          <CompletionDonutChart />
+        </div>
       </div>
-      <div className="col-12 col-sm-6 col-md-5 mt-sm-n3 p-0 text-center">
-        <CompletionDonutChart />
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 CourseCompletion.propTypes = {
   intl: intlShape.isRequired,
