182a21f398b41829ebbdbabad5b71b18e1dbf0c3
qasimgulzar <qasim.khokhar52@gmail.com>
2024-09-27 14:12:03 +0500

    fix: prompt authentication error message when xblock is not publicly accessible.

--- src/courseware/course/sequence/Unit/ContentIFrame.jsx
+++ src/courseware/course/sequence/Unit/ContentIFrame.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ErrorPage } from '@edx/frontend-platform/react';
 import { StrictDict } from '@edx/react-unit-test-utils';
-import { ModalDialog, Modal } from '@openedx/paragon';
+import {
+  ModalDialog, Modal, Icon, PageBanner,
+} from '@openedx/paragon';
 
 import PageLoading from '@src/generic/PageLoading';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import { WarningFilled } from '@openedx/paragon/icons';
 import * as hooks from './hooks';
 
 /**
@@ -26,6 +30,49 @@ export const testIDs = StrictDict({
   contentIFrame: 'content-iframe-test-id',
   modalIFrame: 'modal-iframe-test-id',
 });
+
+const IFrameComponent = ({
+  url, shouldShowContent, hasLoaded, loadingMessage, showError, title, contentIFrameProps,
+}) => {
+  const [httpStatusCode, setHttpStatusCode] = useState(500);
+  useEffect(() => {
+    getAuthenticatedHttpClient().get(url).then(res => {
+      setHttpStatusCode(res.status);
+    }).catch((error) => {
+      setHttpStatusCode(error.response.status);
+    });
+  }, []);
+  return (
+    <>
+      {(shouldShowContent && !hasLoaded) && (
+      // eslint-disable-next-line no-nested-ternary
+        showError ? (httpStatusCode >= 400 && httpStatusCode < 500
+          ? (
+            <PageBanner
+              variant="warning"
+            >
+              <Icon src={WarningFilled} className="mie-2" /> Please signin to view this content
+            </PageBanner>
+          ) : <ErrorPage />) : <PageLoading srMessage={loadingMessage} />
+      )}
+      {shouldShowContent && (
+      <div className="unit-iframe-wrapper">
+        <iframe title={title} {...contentIFrameProps} data-testid={testIDs.contentIFrame} />
+      </div>
+      )}
+    </>
+  );
+};
+IFrameComponent.propTypes = {
+  url: PropTypes.string.isRequired,
+  shouldShowContent: PropTypes.bool.isRequired,
+  hasLoaded: PropTypes.bool.isRequired,
+  loadingMessage: PropTypes.bool.isRequired,
+  showError: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  contentIFrameProps: PropTypes.any.isRequired,
+};
 
 const ContentIFrame = ({
   iframeUrl,
@@ -81,14 +128,15 @@ const ContentIFrame = ({
 
   return (
     <>
-      {(shouldShowContent && !hasLoaded) && (
-        showError ? <ErrorPage /> : <PageLoading srMessage={loadingMessage} />
-      )}
-      {shouldShowContent && (
-        <div className="unit-iframe-wrapper">
-          <iframe title={title} {...contentIFrameProps} data-testid={testIDs.contentIFrame} />
-        </div>
-      )}
+      <IFrameComponent
+        contentIFrameProps={contentIFrameProps}
+        showError={showError}
+        title={title}
+        hasLoaded={hasLoaded}
+        url={iframeUrl}
+        loadingMessage={loadingMessage}
+        shouldShowContent={shouldShowContent}
+      />
       {modalOptions.isOpen && (modalOptions.isFullscreen
         ? (
           <ModalDialog
