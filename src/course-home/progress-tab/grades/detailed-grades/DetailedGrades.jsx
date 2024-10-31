e69114a8396be2575452d299e81b4814cbe77294
Navin Karkera <navin@opencraft.com>
2024-10-31 15:48:24 +0530

    feat: option to show/hide ungraded assignment in progress page (#1380)
    * feat: option to show/hide ungraded assignment in progress page
    
    test: add tests for show ungraded toggle
    
    feat: update score label in progress page based on grading
    
    refactor: update score label text and add tooltip
    
    refactor: move label tooltip near header as normal text
    
    refactor: update problem score label Graded scores
    
    * refactor: move config check to utils

--- src/course-home/progress-tab/grades/detailed-grades/DetailedGrades.jsx
+++ src/course-home/progress-tab/grades/detailed-grades/DetailedGrades.jsx
@@ -7,6 +7,7 @@ import { FormattedMessage, injectIntl, intlShape } from '@edx/frontend-platform/
 import { Blocked } from '@openedx/paragon/icons';
 import { Icon, Hyperlink } from '@openedx/paragon';
 import { useModel } from '../../../../generic/model-store';
+import { showUngradedAssignments } from '../../utils';
 
 import DetailedGradesTable from './DetailedGradesTable';
 
@@ -28,6 +29,8 @@ const DetailedGrades = ({ intl }) => {
   } = useModel('progress', courseId);
 
   const hasSectionScores = sectionScores.length > 0;
+  const emptyTableMsg = showUngradedAssignments()
+    ? messages.detailedGradesEmpty : messages.detailedGradesEmptyOnlyGraded;
 
   const logOutlineLinkClick = () => {
     sendTrackEvent('edx.ui.lms.course_progress.detailed_grades.course_outline_link.clicked', {
@@ -54,7 +57,25 @@ const DetailedGrades = ({ intl }) => {
 
   return (
     <section className="text-dark-700">
-      <h3 className="h4 mb-3">{intl.formatMessage(messages.detailedGrades)}</h3>
+      <h3 className="h4">{intl.formatMessage(messages.detailedGrades)}</h3>
+      <ul className="micro mb-3 pl-3 text-gray-700">
+        <li>
+          <b>{intl.formatMessage(messages.practiceScoreLabel)} </b>
+          <FormattedMessage
+            id="progress.detailedGrades.practice-label.info.text"
+            defaultMessage="Scores from non-graded activities meant for practice and self-assessment."
+            description="Information text about non-graded practice score label"
+          />
+        </li>
+        <li>
+          <b>{intl.formatMessage(messages.gradedScoreLabel)} </b>
+          <FormattedMessage
+            id="progress.detailedGrades.problem-label.info.text"
+            defaultMessage="Scores from activities that contribute to your final grade."
+            description="Information text about graded problem score label"
+          />
+        </li>
+      </ul>
       {gradesFeatureIsPartiallyLocked && (
         <div className="mb-3 small ml-0 d-inline">
           <Icon className="mr-1 mt-1 d-inline-flex" style={{ height: '1rem', width: '1rem' }} src={Blocked} data-testid="blocked-icon" />
@@ -65,9 +86,9 @@ const DetailedGrades = ({ intl }) => {
         <DetailedGradesTable />
       )}
       {!hasSectionScores && (
-        <p className="small">{intl.formatMessage(messages.detailedGradesEmpty)}</p>
+        <p className="small">{intl.formatMessage(emptyTableMsg)}</p>
       )}
-      {overviewTabUrl && (
+      {overviewTabUrl && !showUngradedAssignments() && (
         <p className="x-small m-0">
           <FormattedMessage
             id="progress.ungradedAlert"
