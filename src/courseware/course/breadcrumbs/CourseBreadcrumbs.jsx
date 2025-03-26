e2b54f2e6773e398448d55a0fdf45936902f2864
KristinAoki <kaoki@2u.com>
2025-03-26 16:29:07 -0400

    fix: breadcrumb preview link

--- src/courseware/course/breadcrumbs/CourseBreadcrumbs.jsx
+++ src/courseware/course/breadcrumbs/CourseBreadcrumbs.jsx
@@ -1,107 +1,12 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { getConfig } from '@edx/frontend-platform';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
-import { useToggle, ModalPopup, Menu } from '@openedx/paragon';
 import { Link } from 'react-router-dom';
-import { useModel, useModels } from '../../generic/model-store';
-import JumpNavMenuItem from './JumpNavMenuItem';
-
-const CourseBreadcrumb = ({
-  content,
-  withSeparator,
-  courseId,
-  sequenceId,
-  unitId,
-  isStaff,
-}) => {
-  const defaultContent = content.filter(
-    (destination) => destination.default,
-  )[0] || { id: courseId, label: '', sequences: [] };
-
-  const showRegularLink = getConfig().ENABLE_JUMPNAV !== 'true' || content.length < 2 || !isStaff;
-  const [isOpen, open, close] = useToggle(false);
-  const [target, setTarget] = useState(null);
-  return (
-    <>
-      {withSeparator && (
-        <li className="col-auto p-0 mx-2 text-primary-500 text-truncate text-nowrap" role="presentation" aria-hidden>/</li>
-      )}
-
-      <li
-        style={{
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          whiteSpace: 'nowrap',
-        }}
-        data-testid="breadcrumb-item"
-      >
-        {showRegularLink ? (
-          <Link
-            className="text-primary-500"
-            to={
-              defaultContent.sequences.length
-                ? `/course/${courseId}/${defaultContent.sequences[0].id}`
-                : `/course/${courseId}/${defaultContent.id}`
-            }
-          >
-            {defaultContent.label}
-          </Link>
-        ) : (
-          <>
-            {
-              // eslint-disable-next-line
-              <a className="text-primary-500" onClick={open} ref={setTarget}>
-                {defaultContent.label}
-              </a>
-            }
-            <ModalPopup positionRef={target} isOpen={isOpen} onClose={close}>
-              <Menu>
-                {content.map((item) => (
-                  <JumpNavMenuItem
-                    key={item.label}
-                    isDefault={item.default}
-                    sequences={item.sequences}
-                    courseId={courseId}
-                    title={item.label}
-                    currentSequence={sequenceId}
-                    currentUnit={unitId}
-                    onClick={close}
-                  />
-                ))}
-              </Menu>
-            </ModalPopup>
-          </>
-        )}
-      </li>
-    </>
-  );
-};
-CourseBreadcrumb.propTypes = {
-  content: PropTypes.arrayOf(
-    PropTypes.shape({
-      default: PropTypes.bool,
-      id: PropTypes.string,
-      label: PropTypes.string,
-    }),
-  ).isRequired,
-  sequenceId: PropTypes.string,
-  unitId: PropTypes.string,
-  withSeparator: PropTypes.bool,
-  courseId: PropTypes.string,
-  isStaff: PropTypes.bool,
-};
-
-CourseBreadcrumb.defaultProps = {
-  withSeparator: false,
-  sequenceId: null,
-  unitId: null,
-  courseId: null,
-  isStaff: null,
-};
+import { useModel, useModels } from '../../../generic/model-store';
+import BreadcrumbItem from './BreadcrumbItem';
 
 const CourseBreadcrumbs = ({
   courseId,
@@ -110,14 +15,16 @@ const CourseBreadcrumbs = ({
   unitId,
   isStaff,
 }) => {
-  const course = useModel('coursewareMeta', courseId);
+  const course = useModel('coursewareMeta', courseId);  
   const courseStatus = useSelector((state) => state.courseware.courseStatus);
   const sequenceStatus = useSelector(
     (state) => state.courseware.sequenceStatus,
   );
+  console.log( useModels('sections', course.sectionIds));
+  
 
   const allSequencesInSections = Object.fromEntries(
-    useModels('sections', course.sectionIds).map((section) => [
+    useModels('sections', course.sectionIds)?.map((section) => [
       section.id,
       {
         default: section.id === sectionId,
@@ -152,6 +59,8 @@ const CourseBreadcrumbs = ({
     }
     return [chapters, sequentials];
   }, [courseStatus, sequenceStatus, allSequencesInSections]);
+  console.log(links);
+   
 
   return (
     <nav aria-label="breadcrumb" className="d-inline-block col-sm-10 mb-3">
@@ -171,7 +80,7 @@ const CourseBreadcrumbs = ({
           </Link>
         </li>
         {links.map((content, i) => (
-          <CourseBreadcrumb
+          <BreadcrumbItem
             // eslint-disable-next-line react/no-array-index-key
             key={i}
             courseId={courseId}
@@ -179,6 +88,7 @@ const CourseBreadcrumbs = ({
             content={content}
             unitId={unitId}
             withSeparator
+            separator="/"
             isStaff={isStaff}
           />
         ))}
