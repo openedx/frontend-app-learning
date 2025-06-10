05a4584560fb5bd1b2ed829a7a9e11d56a083e09
jciasenza <jciasenza@aulasneo.com>
2025-06-10 18:18:26 -0300

    fix: test

--- src/course-home/outline-tab/LmsHtmlFragment.test.jsx
+++ src/course-home/outline-tab/LmsHtmlFragment.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import LmsHtmlFragment from './LmsHtmlFragment';
 import { getConfig } from '@edx/frontend-platform';
+import LmsHtmlFragment from './LmsHtmlFragment';
 
 jest.mock('@edx/frontend-platform', () => ({
   getConfig: jest.fn(),
