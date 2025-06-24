884d54544122156b948aa2dc2bebb5783a259be7
Kshitij Sobti <kshitij@sobti.in>
2025-06-24 19:33:30 +0530

    fixup! fixup! fixup! refactor: Use dynamic slot ids for pluggable pages.

--- src/plugin-routes.test.tsx
+++ src/plugin-routes.test.tsx
@@ -1,5 +1,5 @@
 import { getConfig } from '@edx/frontend-platform';
-import { render,screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { getPluginRoutes } from './plugin-routes';
 
