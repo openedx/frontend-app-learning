202e38bc2fac1f220fc13acdb513adb33e639b3e
Jansen Kantor <jkantor@edx.org>
2025-07-29 13:46:56 -0400

    feat: add plugin slot for content iframe error component

---
+++ src/plugin-slots/ContentIFrameErrorSlot/index.tsx
@@ -0,0 +1,15 @@
+import { PluginSlot } from '@openedx/frontend-plugin-framework';
+import { ErrorPage } from '@edx/frontend-platform/react';
+
+export const ContentIFrameErrorSlot : React.FC<Props> = ({courseId}) => (
+  <PluginSlot 
+    id="org.openedx.frontend.learning.content_iframe_error.v1"
+    pluginProps={{courseId}}
+  >
+    <ErrorPage />
+  </PluginSlot>
+);
+
+interface Props {
+    courseId: string;
+}
